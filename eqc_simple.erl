d647f28c5e6a05f71793fa3a80c9c234e3e28434
Lukasz Milewski <lmmilewski@gmail.com>
2011-05-02 15:06:45 +0200

    final version of quickcheck example (eqc_simple.erl)
    add better version of prop_delete_ok
    add better example of collect function usage
    add failing test documentation (fail function usage)
    add note where to find more examples

--- eqc_simple.erl
+++ eqc_simple.erl
@@ -1,11 +1,14 @@
+%%% This is based on eqc/examples/lists_eqc.erl
+
+%% you must run this or you'll get a lot of compilation errors
+%% code:add_patha("eqc/ebin/").
+
 -module(eqc_simple).
-%% -export([basic/0, delete_fail/0, delete_ok/0]).
 -compile([export_all]).
 
 -include_lib("eqc/include/eqc.hrl").
 
-%% you must run this or you'll get a lot of compilation errors
-%% code:add_patha("eqc/ebin/").
+
 
 %% test lists:reverse function
 %% we check that reverse(reverse(X)) is identity
@@ -37,6 +40,12 @@ prop_delete_ok() ->
     ?FORALL({X, Xs}, {int(), list(int())},
             ?IMPLIES(length(lists:filter(fun(E) -> E == X end, Xs)) < 2,
                      not lists:member(X, lists:delete(X, Xs)))).
+prop_delete_ok_2() ->
+    ?FORALL({X, Xs, Ys}, {int(), list(int()), list(int())},
+            ?IMPLIES(not lists:member(X, Xs),
+                     equals(lists:delete(X, Xs ++ [X] ++Ys), % equals is true if arguments are equal
+                            Xs ++ Ys))).
+
 delete_ok() ->
     quickcheck(numtests(10000, prop_delete_ok())).
 
@@ -45,7 +54,10 @@ delete_ok() ->
 %% distribution). Here we check how often X is an element of a given
 %% list
 check_member_probability() ->
-    quickcheck(?FORALL({X,Xs}, {int(), list(int())}, collect(lists:member(X, Xs), true))).
+    quickcheck(?FORALL({X,Xs}, {int(), list(int())},
+                       collect(with_title(member), lists:member(X, Xs),
+                               collect(with_title(length), length(Xs),
+                                       true)))).
 %% OK, passed 100 tests
 %% 96% false
 %% 4% true
@@ -61,4 +73,13 @@ delete_fail_fast() ->
     quickcheck(prop_delete_fail_fast()).
 
 
-%% for more see eqc/examples/lists_eqc.erl example
+%% let's document failing case by creating property that uses eqc:fails
+%% eqc:fails expectes at least one failing case
+prop_fails_delete() ->
+    fails(?FORALL({X, Xs}, {int(), list(int())},
+                  ?IMPLIES(lists:member(X, Xs),
+                           not lists:member(X, lists:delete(X, Xs))))).
+
+%%% for more see other eqc examples in eqc/example/ diretory
+%% make sure to see eqc/example/lists_eqc.erl
+%% look for prop_seq part
